b7dfcc6cb29bc2ea4ca2f3cd5540b895d5127f26
Breno <brenosmmaia@gmail.com>
2024-08-10 17:39:57 -0300

    calculating similarity with process

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,36 +1,39 @@
 import json
 import pandas as pd
 
-from typing import Dict
-from rapidfuzz import fuzz
+from typing import Dict, List, Tuple
+from rapidfuzz import fuzz, process, utils
 
 def read_input() -> Dict[str, str]:
     """Reads the input question from a json file"""
 
     with open("input.json", "r", encoding="utf-8") as file:
         data = json.load(file)
-        data = json.dumps(data, ensure_ascii=False)
     return data
 
 
+def make_json_output(data: List[Tuple[str, float, int]], filename: str="output.json") -> None:
+    """Converts and sorts the output from rapidfuzz.process"""
+
+    sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
+    
+    result = {item[0]: round(item[1], 5) for item in sorted_data}
+    
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
 
 
 def main():
-    question = read_input()
+    question = read_input()["pergunta"]
 
     questions_df = pd.read_excel(io="../data/perguntas_chatbot_v1.xlsx")
     questions_faq = questions_df["perguntas"].tolist()
 
-    results = [fuzz.ratio(question, i) for i in questions_faq]
+    ratio_results = process.extract(question, questions_faq, scorer=fuzz.ratio, limit=None, processor=utils.default_process)
+    token_set_ratio_results = process.extract(question, questions_faq, scorer=fuzz.token_set_ratio, limit=None, processor=utils.default_process)
 
-    questions_faq = dict(zip(questions_faq, results))
-
-    max_key = max(questions_faq, key=questions_faq.get)
-    max_value = questions_faq[max_key]
-
-
-    print(max_key, max_value)
-
+    make_json_output(ratio_results, "output_ratio.json")
+    make_json_output(token_set_ratio_results, "output_token_set_ratio.json")
 
 
 if __name__ == "__main__":
