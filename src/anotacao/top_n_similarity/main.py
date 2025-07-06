8b52c9381e82c443e6c3380f6a1056c47c9d0d75
Breno <brenosmmaia@gmail.com>
2025-07-06 17:11:51 -0300

    updating results

--- src/anotacao/top_n_similarity/main.py
+++ src/anotacao/top_n_similarity/main.py
@@ -29,7 +29,7 @@ def make_output_csv(df: pd.DataFrame, df_faq_users: pd.DataFrame) -> pd.DataFram
 
 def main():
     df_faq_users = pd.read_excel(
-        io="../../../data/Perguntas_chatbot - 09.10_relacao FAQ perguntas users.xlsx",
+        io="../../../data/Perguntas_chatbot_clean - 09.10_relacao FAQ perguntas users.xlsx",
         sheet_name="relacao_clean",
     )
 
